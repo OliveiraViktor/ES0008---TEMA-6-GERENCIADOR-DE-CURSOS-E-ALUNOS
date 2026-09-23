44e7619016b119a6c6e28398fde4659f9c6c0697
Luan Viktor Oliveira <achillesy82@gmail.com>
2026-09-23 00:04:30 +0000

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+class Curso:
+    def __init__(self, codigo, nome, ch, pr, ementa):
+        self.codigo = codigo
+        self.nome = nome
+        self.ch = ch
+        self.pr = pr
+        self.ementa = ementa
+
+c1 = Curso("Engenharia de Software", UFCA001, )
+
+        
+pass
